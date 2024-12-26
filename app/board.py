9bd03c7e68ed6cecbf3d16b1532fcf48ae745753
Tassilo Selover-Stephan <tassilo@selover.net>
2024-12-25 21:20:06 -0800

    added board and suntimes

---
+++ app/board.py
@@ -0,0 +1,202 @@
+#!/usr/bin/env python
+import time, datetime
+import sys, os, subprocess
+import logging
+
+from PIL import Image, ImageDraw, ImageFont
+from traceback import format_exception
+
+sys.path.append("rpi-rgb-led-matrix/bindings/python")
+# from rgbmatrix import RGBMatrix, RGBMatrixOptions
+from RGBMatrixEmulator import RGBMatrix, RGBMatrixOptions
+
+from sun_times import SunDisplayer
+
+# region --- initialise logging ---
+logger = logging.getLogger(__name__)
+handler = logging.StreamHandler(stream=sys.stdout)
+logger.addHandler(handler)
+fh = logging.FileHandler(f'{os.path.dirname(os.path.realpath(__file__))}/mta-board.log')
+fh.setLevel(logging.WARNING)
+logger.addHandler(fh)
+
+def handle_exception(exc_type, exc_value, exc_traceback):
+    if issubclass(exc_type, KeyboardInterrupt):
+        sys.__excepthook__(exc_type, exc_value, exc_traceback)
+        return
+
+    logger.error("Uncaught exception", exc_info=(exc_type, exc_value, exc_traceback))
+
+sys.excepthook = handle_exception
+# endregion
+
+class MatrixBoard():
+    def __init__(self, *args, **kwargs):
+        # Configuration for the matrix
+        options = RGBMatrixOptions()
+        options.rows = 32
+        options.cols = 64
+        options.brightness = 60
+        options.chain_length = 1
+        options.parallel = 1
+        options.hardware_mapping = 'adafruit-hat-pwm'  # If you have an Adafruit HAT: 'adafruit-hat'
+        options.gpio_slowdown = 3
+        # options.pwm_lsb_nanoseconds = 130
+        options.limit_refresh_rate_hz = 160
+        options.show_refresh_rate = 0
+
+        # EMLUTATOR SETTINGS
+        options.pixel_style = 'circle'
+        
+        self.dir = os.path.dirname(os.path.realpath(__file__)) # directory of this file
+        # self.logo = Image.open(f'{self.dir}/assets/images/L_logo.png').convert('RGBA') # pull logo from the assets
+
+        self.matrix = RGBMatrix(options = options) # init the matrix with its options
+        
+        self.running = False # start as not running
+        self.runTime = 0
+        self.i = 0 # number of updates that have been ticked
+
+    def run(self):
+        self.startTime = datetime.datetime.now()
+        # self.font = ImageFont.truetype(f'{self.dir}/assets/fonts/04B_03__.TTF', size=8)
+        # self.timeFont = ImageFont.truetype(f'{self.dir}/assets/fonts/luminator-4x6-dotmap.ttf', size=6, )
+        self.displayer = SunDisplayer(45.428097, -122.681340)
+        self.lastTodayUpdate: float = 0.0
+
+        self.canvas = self.matrix.CreateFrameCanvas()
+
+        self.running = True # start things up
+        while self.running:
+            runTime:datetime.timedelta = datetime.datetime.now() - self.startTime # get runtime as timedelta
+            self.runTime:float = runTime.seconds + runTime.microseconds * 10.0**-6 # convert runtime to sec
+            self.image = Image.new('RGBA', (self.matrix.width, self.matrix.height)) # new PIL image to build on
+            # -- reload current time every 10 seconds --
+            if self.runTime - self.lastTodayUpdate > 10:
+                self.displayer.today = datetime.datetime.today()
+                self.lastTodayUpdate = self.runTime
+            # -- reload sunData when the day changes --
+            if self.displayer.todayData.date.day != self.displayer.today.day:
+                self.displayer.reloadData()
+            self.displayer.showSun = False if int(self.runTime) % 2 == 0 else True
+
+            sunImage = self.displayer.generateImage()
+            self.image.paste(sunImage, (0,0))
+
+
+            self.canvas.Clear() # clear whatever's on the screen
+            self.canvas.SetImage(self.image.convert('RGB'), 0, 0) # transfer the PIL image to the canvas
+
+            self.i += 1 # increment the number of iterations run
+            time.sleep(0.005) # the nominal fastest tick time
+            self.canvas = self.matrix.SwapOnVSync(self.canvas) # pulls in the just-generated canvas on the next frame
+
+
+    def showIP(self):
+        """
+        Show the machine's wifi IP on the screen
+        """
+        ip: str = subprocess.check_output(['ipconfig getifaddr en0'], encoding='UTF-8')
+        ipBox = TextBox(text=ip, font=self.font, x=0, y=-1)
+        ipBox.addToImage(self.image, (1,1))
+
+    # def showTime(self):
+    #     t1 = datetime.datetime.now().strftime("%d.%m.%Y")
+    #     t2 = datetime.datetime.now().strftime("%H:%M:%S")
+    #     timeBox1 = TextBox(text=t1, font=self.font)
+    #     timeBox1.addToImage(self.image, (5, 5))
+    #     timeBox2 = TextBox(text=t2, font=self.font)
+    #     timeBox2.addToImage(self.image, (5, 15))        
+
+class TextBox:
+    """
+    Represents a text box that can be added to the board.
+    self.outputImage and self.addToImage are the best ways to get the text
+
+    Parameters:
+        text (str): the text to show
+        font (ImageFont.FreeTypeFont): your font of choice
+        x (int): shift the font within the text box in x. Defaults to 0
+        y (int): shift the font within the text box in y. Defaults to 0
+        width (int): Optional argument to force the box width (good for scrolling text)
+        height (int): Optional argument to force the box height (good for weird fonts)
+        type (str): can be 'static', 'scrolling', or 'blinking'
+        i (int): link this to something that ticks at the fequency you want animations to update at
+    """
+    def __init__(self,
+                 text:str,
+                 font:ImageFont.FreeTypeFont,
+                 fill:str|None = None,
+                 x:int=None,
+                 y:int=None,
+                 width:int=None,
+                 height:int=None,
+                 type:str='static',
+                 i:int=0,
+                 ):
+        
+        self.text = str(text)
+        self.font = font
+        self.type = type
+        self.fill = fill
+        self.i = i
+        self.holdSteps = 40 # ticks to hold between scrolling
+
+        boundingBox = self.font.getmask(self.text).getbbox()
+        self.textWidth = boundingBox[2]
+        self.textHeight = boundingBox[3]
+        self.width = width if width else self.textWidth # default to the calculated width if no arg given
+        self.height = height if height else self.textHeight # default to the calculated height if no arg given
+
+        self.x = x if x else 0 # default to the zero offset if no arg given
+        self.y = y if y else 0 # default to the zero offset if no arg given
+
+        match font.font.family: # some defaults for fonts I know the offsets of
+            case '04b03':
+                self.y = y if y else -1 # default to the -1 offset if no arg given
+
+        self.outputImage = Image.new('RGBA', (self.width, self.height)) # blank image to draw on
+        self.outputDraw = ImageDraw.Draw(self.outputImage) # draw object to use for adding text
+
+        match self.type:
+            case 'scrolling':
+                self.showScrollingText()
+            case 'blinking':
+                if int(self.i)%2: # show text every other tick
+                    self.showStaticText()
+            case _:
+                self.showStaticText()
+
+    def showStaticText(self):
+        self.outputDraw.text((self.x, self.y), self.text, fill=self.fill, font=self.font)
+
+    def showScrollingText(self):
+        spacing = int(self.textHeight * 2) # this is a nice amount of spacing. I'm hardcoding it!
+        scrollWidth = self.textWidth + spacing # width of the scrolling element with its spacing
+        
+        if self.textWidth < self.width: # if there's no point in scrolling because the whole text fits
+            self.showStaticText() # just show the text
+            return None
+
+        positions = [-2*scrollWidth, -scrollWidth, 0] # create some instances that can scroll into the screen
+        posAdjustment = self.i % (scrollWidth + self.holdSteps) # the amount to scoot each instance over by each time
+        # the line above does a modulo of the scrollwidth because restarting after that many steps is seamless.
+        # holdSteps just makes the scrolling hesitate so it's easier to read
+
+        for pos in positions: # for each instance of the text
+            if posAdjustment <= scrollWidth: # only shift the text for one unit of scrolling
+                pos += posAdjustment # shift text to the right
+            self.outputDraw.text((pos, self.y), self.text, fill=self.fill, font=self.font)
+    
+    def addToImage(self, imgObj:ImageDraw.ImageDraw, pos:tuple[int, int]):
+        imgObj.paste(im=self.outputImage, box=pos) # plop the textbox image into the parent image at the given position
+        
+
+if __name__ == "__main__":
+    board = MatrixBoard()
+    while True:
+        try:
+            board.run()
+        except Exception as ex:
+            logging.error(''.join(format_exception(None, ex, ex.__traceback__)))
+            continue
---
+++ app/sun_times.py
@@ -0,0 +1,105 @@
+from typing import Dict, Tuple
+from dataclasses import dataclass
+from datetime import datetime, timedelta
+import math
+
+import requests
+from PIL import Image, ImageDraw, ImageFont
+
+@dataclass
+class SunData():
+    date: datetime
+    sunrise: datetime
+    sunset: datetime
+    first_light: datetime
+    last_light: datetime
+    dawn: datetime
+    dusk: datetime
+    golden_hour: datetime
+    day_length: int
+
+class SunDisplayer():
+    def __init__(self, lat, lon):
+        self.coords: Tuple[float] = (lat, lon)
+        self.font = ImageFont.truetype(f'assets/fonts/04B_03__.TTF', size=8)
+
+        self.today: datetime = datetime.today()
+        tomorrow = self.today + timedelta(days=1)
+        self.todayData: SunData = getSunData(self.coords[0], self.coords[1])
+        self.tomorrowData: SunData = getSunData(self.coords[0], self.coords[1], tomorrow)
+
+        self.showSun: bool = True
+
+    def generateImage(self) -> Image.Image:
+        image: Image.Image = Image.new('RGBA', (64, 32)) # new PIL image to build on
+        draw: ImageDraw.ImageDraw = ImageDraw.Draw(image) # draw object to use for adding text
+        
+        self._drawText(draw)
+        self._drawArcs(draw)
+        if self.showSun: self._drawSun(draw)
+
+        return image
+
+    def reloadData(self):
+        self.todayData: SunData = getSunData(self.coords[0], self.coords[1])
+        tomorrow = self.today + timedelta(days=1)
+        self.tomorrowData: SunData = getSunData(self.coords[0], self.coords[1], tomorrow)
+
+    def _drawText(self, draw: ImageDraw.ImageDraw):
+        if self.todayData.sunrise < self.today:
+            topLabel: str = "Sunset"
+            topText: str = self.todayData.sunset.strftime('%H:%M')
+            bottomLabel: str = "Sunrise"
+            bottomText: str = self.tomorrowData.sunrise.strftime('%H:%M')
+        else:
+            topLabel: str = "Sunrise"
+            topText: str = self.todayData.sunrise.strftime('%H:%M')
+            bottomLabel: str = "Sunset"
+            bottomText: str = self.todayData.sunset.strftime('%H:%M')
+
+        draw.text((33, 1), topLabel, fill=None, font=self.font)
+        draw.text((33, 8), topText, fill=None, font=self.font)
+        draw.text((33, 17), bottomLabel, fill=None, font=self.font)
+        draw.text((33, 24), bottomText, fill=None, font=self.font)
+
+    def _drawArcs(self, draw: ImageDraw.ImageDraw):
+        day_arc = self.todayData.day_length / (24*60*60) * 360
+        day_arc_offset = (180-day_arc)/2
+        draw.arc(((1,1),(30,30)), day_arc_offset-180, -day_arc_offset, fill='#57b9fa', width=2)
+        draw.arc(((1,1),(30,30)), -day_arc_offset, day_arc_offset-180, fill='#7a7a7a', width=2)
+
+    def _drawSun(self, draw: ImageDraw.ImageDraw):
+        midnight = self.today.replace(hour=0, minute=0, second=0, microsecond=0)
+        sun_theta = (self.today - midnight).seconds / (24*60*60) * 2*math.pi
+        sun_x = int(16 - 14*math.sin(sun_theta))
+        sun_y = int(16 + 14*math.cos(sun_theta))
+
+        draw.point((sun_x,sun_y), fill='#ffe921')
+        draw.point((sun_x+1,sun_y), fill=(255, 233, 33, 70))
+        draw.point((sun_x-1,sun_y), fill=(255, 233, 33, 70))
+        draw.point((sun_x,sun_y+1), fill=(255, 233, 33, 70))
+        draw.point((sun_x,sun_y-1), fill=(255, 233, 33, 70))
+
+
+def getSunData(lat:float, lon:float, date:datetime=datetime.today()) -> SunData:
+    url: str = f"https://api.sunrisesunset.io/json?lat={lat:0.6f}&lng={lon:0.6f}&date={date.strftime('%Y-%m-%d')}"
+    r = requests.get(url)
+    if r.status_code != 200: return None
+    if r.headers['content-type'] != "application/json": return None
+    results: Dict[str, str] = r.json().get('results')
+
+    data_date: datetime = datetime.strptime(results['date'], "%Y-%m-%d")
+    day_len: int = int(results['day_length'].split(':')[0]) * 3600 + int(results['day_length'].split(':')[1]) * 60 + int(results['day_length'].split(':')[2])
+
+    data = SunData(
+        date=data_date,
+        sunrise=datetime.strptime(f"{results['date']} {results['sunrise']}", "%Y-%m-%d %I:%M:%S %p"),
+        sunset=datetime.strptime(f"{results['date']} {results['sunset']}", "%Y-%m-%d %I:%M:%S %p"),
+        first_light=datetime.strptime(f"{results['date']} {results['first_light']}", "%Y-%m-%d %I:%M:%S %p"),
+        last_light=datetime.strptime(f"{results['date']} {results['last_light']}", "%Y-%m-%d %I:%M:%S %p"),
+        dawn=datetime.strptime(f"{results['date']} {results['dawn']}", "%Y-%m-%d %I:%M:%S %p"),
+        dusk=datetime.strptime(f"{results['date']} {results['dusk']}", "%Y-%m-%d %I:%M:%S %p"),
+        golden_hour=datetime.strptime(f"{results['date']} {results['golden_hour']}", "%Y-%m-%d %I:%M:%S %p"),
+        day_length=day_len,)
+    return data
+
