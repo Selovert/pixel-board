e0392649d1e327d42daa9b9aacebfcb0f6683542
Tassilo Selover-Stephan <tassilo@selover.net>
2025-01-19 15:46:25 -0800

    pdm

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 # -- install python bindings --
-sudo apt-get update && sudo apt-get install python3-dev cython3 -y
-(cd /home/dietpi/Projects/pixel-board/app/rpi-rgb-led-matrix/ && sudo make build-python)
-(cd /home/dietpi/Projects/pixel-board/app/rpi-rgb-led-matrix/ && sudo make install-python)
+# sudo apt-get update && sudo apt-get install python3-dev cython3 -y
+# (cd app/rpi-rgb-led-matrix/ && sudo make build-python)
+# (cd app/rpi-rgb-led-matrix/ && sudo make install-python)
+
+sudo pdm install
+pdm export -o requirements.txt --without-hashes
+sudo pip install -r requirements.txt
