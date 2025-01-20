137abf975ddde561a459dfecce63e5541953c959
Tassilo Selover-Stephan <tassilo@selover.net>
2025-01-19 22:14:52 -0800

    install script

--- install.sh
+++ install.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-# -- install python bindings --
-# sudo apt-get update && sudo apt-get install python3-dev cython3 -y
-# (cd app/rpi-rgb-led-matrix/ && sudo make build-python)
-# (cd app/rpi-rgb-led-matrix/ && sudo make install-python)
-
+sudo apt-get update && sudo apt-get install -y build-essential python3-dev cython3 python python3.11-venv pip
+# -- install PDM --
+curl -sSL https://pdm-project.org/install-pdm.py | python3 -
+export PATH=/dietpi/.local/bin:$PATH
+# -- clone the repo and cd in --
+git clone --recurse-submodules https://github.com/Selovert/pixel-board
+cd pixel-board
+# -- install python dependencies --
 sudo pdm install
 pdm export -o requirements.txt --without-hashes
-sudo pip install -r requirements.txt
+sudo pip install -r requirements.txt --break-system-packages
+# -- install python bindings --
+(cd app/rpi-rgb-led-matrix/ && sudo make build-python)
+(cd app/rpi-rgb-led-matrix/ && sudo make install-python)
