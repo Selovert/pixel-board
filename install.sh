345254eb49d7d080a7830aae7eef6761d8f93533
Tassilo Selover-Stephan <tassilo@selover.net>
2025-01-19 22:32:07 -0800

    python -> python3, pip to python3-pip. Verbosity

--- install.sh
+++ install.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-sudo apt-get update && sudo apt-get install -y build-essential python3-dev cython3 python python3.11-venv pip
+echo "------ Installing dependencies ------"
+sudo apt-get update && sudo apt-get install -y build-essential python3-dev cython3 python3 python3.11-venv python3-pip
 # -- install PDM --
+echo "------ Installing PDM ------"
 curl -sSL https://pdm-project.org/install-pdm.py | python3 -
 export PATH=/dietpi/.local/bin:$PATH
-# -- install python dependencies --
+echo "------ Building PDM lock ------"
 cd /usr/local/pixel-board
 sudo pdm install
+echo "------ Installing python packages ------"
 pdm export -o requirements.txt --without-hashes
 sudo pip install -r requirements.txt --break-system-packages
-# -- install python bindings --
+echo "------ Build rpi matrix python bindings ------"
 (cd app/rpi-rgb-led-matrix/ && sudo make build-python)
 (cd app/rpi-rgb-led-matrix/ && sudo make install-python)
