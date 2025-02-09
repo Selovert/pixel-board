aa600952de5be04102ff7deabdab19be59b5264b
Tassilo Selover-Stephan <tassilo@selover.net>
2025-02-08 20:23:09 -0800

    add daily restart

--- install.sh
+++ install.sh
@@ -5,13 +5,15 @@ sudo apt-get update && sudo apt-get install -y build-essential python3-dev cytho
 # -- install PDM --
 echo "------ Installing PDM ------"
 curl -sSL https://pdm-project.org/install-pdm.py | python3 -
-export PATH=/dietpi/.local/bin:$PATH
+export PATH=~/.local/bin:$PATH
 echo "------ Building PDM lock ------"
 cd /usr/local/pixel-board
-sudo pdm install
+pdm install
 echo "------ Installing python packages ------"
 pdm export -o requirements.txt --without-hashes
 sudo pip install -r requirements.txt --break-system-packages
 echo "------ Build rpi matrix python bindings ------"
 (cd app/rpi-rgb-led-matrix/ && sudo make build-python)
 (cd app/rpi-rgb-led-matrix/ && sudo make install-python)
+echo "------ Add nightly restart to crontab ------"
+(sudo crontab -l 2>/dev/null; echo "30 2   *   *   *    /sbin/shutdown -r +5") | sudo crontab -
