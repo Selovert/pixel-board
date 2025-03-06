a1913ff0ea66c5604d03b0877cb36366c6de62b8
Tassilo Selover-Stephan <tassilo@selover.net>
2025-03-05 22:31:31 -0800

    break out && statement for logging

--- Automation_Custom_Script.sh
+++ Automation_Custom_Script.sh
@@ -2,4 +2,5 @@
 #!/bin/bash
 sudo apt-get update && sudo apt-get install -y git
 (cd /usr/local && git clone --recurse-submodules https://github.com/Selovert/pixel-board)
-(cd /usr/local/pixel-board && ./install.sh)
+cd /usr/local/pixel-board
+./install.sh
