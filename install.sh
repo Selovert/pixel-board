d103b9f2dd187ebee9ee0f52bafe23d831c886a0
Tassilo Selover-Stephan <tassilo@selover-stephan.com>
2025-11-08 10:50:18 -0800

    python version massaging

--- install.sh
+++ install.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 echo "------ Installing dependencies ------"
-sudo apt-get update && sudo apt-get install -y build-essential python3-dev cython3 python3 python3.11-venv python3-pip
+sudo apt-get update
+sudo apt-get install -y build-essential python3-dev cython3 python3 python3-venv python3-pip
 # -- install uv --
 echo "------ Installing uv ------"
 curl -LsSf https://astral.sh/uv/install.sh | sh
-export PATH=~/.local/bin:$PATH
-echo "------ installing venv ------"
+source $HOME/.local/bin/env
+echo "------ processing dependencies ------"
 cd /usr/local/pixel-board
-uv lock --no-dev
-echo "------ Installing python packages ------"
+uv lock
 uv export --no-dev --no-hashes > requirements.txt
+echo "------ Installing python packages ------"
 sudo pip install -r requirements.txt --break-system-packages
 echo "------ Build rpi matrix python bindings ------"
 (cd app/rpi-rgb-led-matrix/ && sudo make build-python)
