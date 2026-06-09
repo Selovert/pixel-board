430f2b06c10fc309ef4a7af42665c330f4a09842
Tassilo Selover-Stephan <tassilo@selover-stephan.com>
2026-06-09 14:30:04 -0700

    feat: clone shallow during setup

--- Automation_Custom_Script.sh
+++ Automation_Custom_Script.sh
@@ -3,6 +3,6 @@
 set -e
 
 sudo apt-get update && sudo apt-get install -y git
-(cd /usr/local && git clone --recurse-submodules https://github.com/Selovert/pixel-board)
+(cd /usr/local && git clone --depth 1 --recurse-submodules --shallow-submodules https://github.com/Selovert/pixel-board)
 cd /usr/local/pixel-board
 ./install.sh
